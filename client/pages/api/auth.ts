9a6e9d8bf1176d8ad98dc206dafb50dc85284f11
tonyw0527 <tonyw0527@gmail.com>
2021-02-01 20:32:22 +0900

    Updated auth.ts for admin auth

--- client/pages/api/auth.ts
+++ client/pages/api/auth.ts
@@ -2,7 +2,7 @@
 export default (req, res) => {
   const { invicode } = req.cookies;
 
-  if(invicode === process.env.INVITATION_CODE){
+  if(invicode === process.env.INVITATION_CODE || process.env.ADMIN_SECRET){
     res.status(200).json({success: true});
   } else {
     res.statsus(400).json({success: false});
